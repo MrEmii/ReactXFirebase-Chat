1df528ccc7278bafbfb3f418df82acb2d052d38c
Emir <32276305+MrEmii@users.noreply.github.com>
2018-12-13 16:27:31 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import AppRoutes from './routes';
 import firebase from 'firebase';
 
 firebase.initializeApp({
-    apiKey: "AIzaSyAoY7kpVGuGg2zsanOKuYLMWL64PoiR3jw",
-    authDomain: "reactxfirebasemremii.firebaseapp.com",
-    databaseURL: "https://reactxfirebasemremii.firebaseio.com",
-    projectId: "reactxfirebasemremii",
-    storageBucket: "reactxfirebasemremii.appspot.com",
-    messagingSenderId: "778537838188"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 })
 
 render(
